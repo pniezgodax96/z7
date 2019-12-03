0e7a64649f14c9cbe345d3936c9a1be3dc55cc5d
pniezgodax96 <pniezgoda96@gmail.com>
2019-12-03 23:05:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>Niezgoda</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+</head>
+
+<body>
+<br>
+<b>Zaloguj się:</b><br><br>
+<form method="post" action="zaloguj.php">
+ Login:<input type="text" name="user"><br><br></br>
+ Haslo:<input type="password" name="pass"><br><br>
+ <input type="submit" value="Wyślij"/>
+</form>
+<a href="rejestruj.php">Nie masz konta? Rejestracja!</a></p>
+
+</body>
+</html>
+</html>
